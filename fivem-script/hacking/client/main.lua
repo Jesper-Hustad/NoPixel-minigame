604b5cf3b9c0c0ac4d9abcff9103ab8a87c1ad95
RealDebugg <52179011+RealDebugg@users.noreply.github.com>
2021-10-04 11:06:43 +0200

    removed old code

--- fivem-script/hacking/client/main.lua
+++ fivem-script/hacking/client/main.lua
@@ -1,39 +1,3 @@
---[[ local IsHacking = false
-
-AddEventHandler('open:minigame', function(callback)
-    Callbackk = callback
-    openHack()
-end)
-
-function OpenHackingGame(callback)
-    Callbackk = callback
-    openHack()
-end
-
-RegisterNUICallback('callback', function(data, cb)
-    closeHack()
-    Callbackk(data.success)
-    cb('ok')
-end)
-
-function openHack()
-    SetNuiFocus(true, true)
-    SendNUIMessage({
-        action = "open"
-    })
-    IsHacking = true
-end
-
-function closeHack()
-    SetNuiFocus(false, false)
-    IsHacking = false
-end
-
-function GetHackingStatus()
-    return IsHacking
-end ]]
-
-
 local successCb
 local failCb
 local resultReceived = false
