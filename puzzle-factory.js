a0b31f10f5a3413e50fce535f1f91dc227947474
qwewas123 <99358004+qwewas123@users.noreply.github.com>
2022-02-10 22:06:52 -0600

    Laptop Hack Change as discussed
    I randomized whether shape text or color text appears on top or on the bottom of the shape. Also, I added the device booting up page at the beginning of the hack.

---
+++ puzzle-factory.js
@@ -0,0 +1,127 @@
+import { randomInt, sample } from './helpers.js'
+
+import TRANSLATIONS from './language.js'
+
+const selectedLang = TRANSLATIONS.SELECTED_LANGUAGE
+
+if(!TRANSLATIONS.LANGUAGES.includes(selectedLang)) console.log(`LANGUAGE NOT SUPPORTED\nSELECTED: ${TRANSLATIONS.SELECTED_LANGUAGE}\nAVAILABLE: ${TRANSLATIONS.LANGUAGES}`)
+const LANG = TRANSLATIONS[selectedLang]
+
+const SHAPES = ["square", "triangle", "rectangle", "circle"]
+const COLORABLE = ['background', 'colortext', 'shapetext', 'number', 'shape']
+
+const COLOR_CODES = ['black', 'white','#1991F9','#8C0C00','#FFE335','#FF9900','#46A04F','#A43AB5']
+
+const LANG_COLORS = LANG.COLORS.reduce((obj, key, i) => {obj[key] = COLOR_CODES[i]; return obj}, {})
+
+
+// console.log('colors var', COLORS)
+// COLORS becomes this:
+const COLORS = {
+    'black' : 'black',
+    'white' : 'white',
+    'blue' : '#1991F9',
+    'red' : '#8C0C00',
+    'yellow' : '#FFE335',
+    'orange' : '#FF9900',
+    'green' : '#46A04F',
+    'purple' : '#A43AB5',
+}
+
+// functions that return answers from PuzzleData class
+const QUESTIONS = {
+    'background color' : (d) => d.colors['background'],
+    'color text background color' : (d) => d.colors['colortext'],
+    'shape text background color' : (d) => d.colors['shapetext'],
+    'number color' : (d) => d.colors['number'],
+    'shape color' : (d) => d.colors['shape'],
+    'color text' : (d) => d.text[0],
+    'shape text' : (d) => d.text[1],
+    'shape' : (d) => d.shape
+}
+
+class PuzzleData {
+    constructor(shape, number, text, colors) {
+      this.shape = shape
+      this.number = number
+      this.text = text
+      this.colors = colors
+    }
+}
+
+// generates a random puzzle
+export function generateRandomPuzzle(){
+
+    const shape = sample(SHAPES)
+    const number = randomInt(9) + 1
+
+    // randomizes whether shape text or color text appears on the top or bottom
+    const rand1 = randomInt(9) + 1
+    if(rand1 % 2 == 0){
+        const topText = sample(Object.keys(LANG_COLORS))
+        const bottomText = sample(SHAPES)
+    }
+    if(rand1 % 2 == 1){
+        const topText = sample(SHAPES)
+        const bottomText = sample(Object.keys(LANG_COLORS))
+    }
+
+    const colors = COLORABLE.reduce((obj, color) => {obj[color] = sample(Object.keys(COLORS)); return obj}, {})
+
+    // ensure color and shape text don't blend with background
+    while(['colortext', 'shapetext'].map(i => colors[i]).includes(colors['background']))
+        colors['background'] = sample(Object.keys(COLORS))
+
+    // ensure nothing blends with shape
+    while(['background', 'colortext', 'shapetext', 'number'].map(i => colors[i]).includes(colors['shape']))
+        colors['shape'] = sample(Object.keys(COLORS))
+
+    return new PuzzleData(shape, number, [topText, bottomText], colors)
+}
+
+
+export function generateQuestionAndAnswer(nums, puzzles){
+
+    const positionOne = randomInt(nums.length)
+    let tempPosTwo
+    do {tempPosTwo = randomInt(nums.length)} while(positionOne == tempPosTwo)
+    const positionTwo = tempPosTwo
+
+    const firstQuestion = sample(Object.keys(QUESTIONS))
+    let tempSecondQuestion
+    do {tempSecondQuestion = sample(Object.keys(QUESTIONS))} while(tempSecondQuestion == firstQuestion)
+    const secondQuestion = tempSecondQuestion
+
+    const andWord = 'AND'
+
+    puzzles = puzzles.map(convertPuzzleDataLang)
+
+    const question =  firstQuestion+' ('+nums[positionOne]+') '+andWord+' '+secondQuestion+' ('+nums[positionTwo]+')'
+    const answer = QUESTIONS[firstQuestion](puzzles[positionOne]) + ' ' + QUESTIONS[secondQuestion](puzzles[positionTwo])
+
+    return [question, answer]
+}
+
+
+// LANGUAGE TRANSLATION FUNCTIONS
+// Should implement a more robust method for all text, but this is a start
+
+// takes in a puzzleData class and converts language of colors
+function convertPuzzleDataLang(puzzle){
+    const result = puzzle
+    puzzle.colors.background = convertColor(puzzle.colors.background)
+    puzzle.colors.number = convertColor(puzzle.colors.number)
+    puzzle.colors.shape = convertColor(puzzle.colors.shape)
+    puzzle.colors.colortext = convertColor(puzzle.colors.colortext)
+    puzzle.colors.shapetext = convertColor(puzzle.colors.shapetext)
+    puzzle.text = puzzle.text.map(i => isColor(i) ? convertColor(i) : i)
+    return result
+}
+
+const isColor = (string) => TRANSLATIONS.EN.COLORS.includes(string)
+
+function convertColor(originalColor){
+    const englishColors = TRANSLATIONS.EN.COLORS
+    const position = englishColors.indexOf(originalColor)
+    return LANG.COLORS[position]
+}
---
+++ script.js
@@ -0,0 +1,88 @@
+"use strict"
+
+// langue française = 'FR'
+// lengua española = 'ES'
+// lingua italiana = 'IT'
+// english language = 'EN'
+export const LANGUAGE_OPTION = 'EN'
+
+import { $, delay, playSound } from './helpers.js'
+import { doPuzzle } from './puzzle-handler.js'
+
+// runs on site load and handles entire  flow
+async function start(){
+
+    // reset from previous
+    $('.try-again').classList.add('hidden')
+    $('.spy-icon').src = 'assets/spy-icon.png'
+
+    playSound('assets/dialing.mp3', 0.1)
+
+    // mock loading screen
+    setInformationText('DEVICE BOOTING UP...')
+    await delay(1)
+    setInformationText('ESTABLISHING CONNECTION...')
+    await delay(1)
+    setInformationText('DOING SOME HACKERMANS STUFF...')
+    await delay(1)
+    setInformationText('ACCESS CODE FLAGGED; REQUIRES HUMAN CAPTCHA INPUT...')
+    await delay(2)
+
+    // hide text and show squares
+    $('#text-container').classList.toggle('hidden')
+    $('#number-container').classList.toggle('hidden')
+
+
+    // activate puzzle 4 times, break on fail
+    let submitted
+    let answer
+    let result = true
+
+    for (let i = 0; i < 4 && result; i++) {
+        [submitted, answer] = await doPuzzle()
+        result = (submitted?.toLowerCase() == answer)
+    }
+
+    // hide squares and show text
+    $('.answer-section').classList.add('hidden')
+    $('.number-container').classList.add('hidden')
+    $('#text-container').classList.remove('hidden')
+    
+    // display result
+    setInformationText((result) ? 'The system has been bypassed.' : "The system didn't accept your answers.")
+    if(!result) $('.spy-icon').src = 'assets/failed.png'
+
+    $('#answer-reveal').textContent = answer
+
+    $('#submitted-reveal').textContent = (result) ?             'Good job, indeed the' :
+                                        ((submitted == null) ?  "The time ran out," : 
+                                                                `You wrote "${submitted || ' '}", the`)
+
+    $('.try-again').classList.remove('hidden')
+}
+
+
+function setInformationText(text){
+    
+    const capitalized = text.toUpperCase()
+    const infoText = `<span class="capital">${capitalized.charAt(0)}</span>${capitalized.substring(1)}`
+    
+    $("#loading-text").innerHTML = infoText
+}
+
+
+// count visitors
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-7E64QM2WXT');
+
+// help menu
+const overlay = $('#overlay')
+$('#help-on').addEventListener('click', () => overlay.style.display = "block")
+$('#overlay').addEventListener('click', () => overlay.style.display = "none")
+
+
+$('#try-again-button').addEventListener('click', start)
+
+start()
